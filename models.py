fba3ed5652dec6bcc81cf1c7639eb4933be452c5
sktrinh12 <strkinh12@gmail.com>
2026-05-11 18:56:47 -0400

    pass client flag

--- models.py
+++ models.py
@@ -1,11 +1,13 @@
 from dataclasses import dataclass
 from datetime import date, time
 
+
 @dataclass
 class TimeEntry:
     work_date: date
     start_time: time
     end_time: time
     hours: float
+    client: str
     category: str = ""
     description: str = ""
